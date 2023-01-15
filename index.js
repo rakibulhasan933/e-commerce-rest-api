7f4247566893bf438b3e29856d0bc8c7a65f3472
RAKIBUL <rakibulmd933@gmail.com>
2023-01-15 23:54:37 +0600

    [ADD] product saved database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config();
 
 
 const app = express();
-const port = 3000;
+const port = process.env.DB_PORT || 5000;
 
 
 app.use(cors());
@@ -13,14 +13,28 @@ app.use(express.json());
 
 // Database Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2kmaoa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-	const collection = client.db("test").collection("devices");
-	console.log('Database Connected');
-	// perform actions on the collection object
-	// client.close();
-});
+
+async function run() {
+	try {
+		const productsCollection = client.db("walker-shop").collection("products");
+		console.log('Database Connected');
+
+		// POST  Products Add
+		app.post('/products', async (req, res) => {
+			const product = req.body;
+			const result = await productsCollection.insertOne(product);
+			res.send(result);
+		});
+
+		// GET Products
+
+
+	} finally {
+		// client.close();
+	}
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
