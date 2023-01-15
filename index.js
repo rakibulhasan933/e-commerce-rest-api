c529801343bf5138b53e478ad0d46230d757b5b3
RAKIBUL <rakibulmd933@gmail.com>
2023-01-15 22:58:44 +0600

    [ADD] database connected

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 
 const app = express();
 const port = 3000;
+
+
+app.use(cors());
+app.use(express.json());
+
+// Database Connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t2kmaoa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+	const collection = client.db("test").collection("devices");
+	console.log('Database Connected');
+	// perform actions on the collection object
+	// client.close();
+});
 
 
 app.get('/', (req, res) => {
